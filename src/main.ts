56d4fdb7ebb8fa67e180eb1af06c678e860c68bb
Axel García <axldorian3@gmail.com>
2023-04-15 15:00:25 -0600

    Change on field dimensions

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
-import './style.css';
-import {Application, Texture, Sprite} from 'pixi.js';
-import fieldUrl from './assets/field.jpg';
+import "./style.css";
+import { Application, Texture, Sprite } from "pixi.js";
+import fieldUrl from "./assets/field.jpg";
 
 const Main = () => {
 	// Elemento HTML donde se va a agregar el canvas de PixiJS
-	const section = document.getElementById('field') as HTMLElement;
+	const section = document.getElementById("field") as HTMLElement;
 
 	// Crear la aplicación de PixiJS
 	const app = new Application({
-		width: section.offsetWidth,
-		height: section.offsetHeight,
+		resizeTo: section,
 	});
 
 	// Agregar el canvas de PixiJS al elemento HTML
@@ -17,7 +16,7 @@ const Main = () => {
 
 	// Agregar el campo de juego (background)
 	const background = Texture.from(fieldUrl);
-	
+
 	// Crear el campo de juego
 	const field = new Sprite(background);
 	field.width = app.screen.width;
@@ -25,7 +24,6 @@ const Main = () => {
 
 	// Agregar el campo de juego al stage
 	app.stage.addChild(field);
-
 };
 
 Main();
